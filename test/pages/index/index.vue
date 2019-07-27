49ee7cce31a7d8482e6c5a09fc06889cf609bf47
YiusingCheung <43678956+Yiusing@users.noreply.github.com>
2019-07-27 11:32:53 +0800

    test

---
+++ test/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+		<button type="primary" @click="toPopup">Popup</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			toPopup() {
+				uni.navigateTo({
+					url:"../popup/popup"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding-top:10rpx;
+	}
+
+</style>
---
+++ test/pages/popup/popup.vue
@@ -0,0 +1,83 @@
+<template>
+	<view>
+		<button type="button" @click="togglePopup('middle-list')">居中弹出（滚动列表）</button>
+		<view class="content">
+			<text>content</text>
+		</view>
+		<uni-popup :show="type === 'middle-list'" position="middle" mode="fixed" @hidePopup="togglePopup('')">
+			<view class="">
+				标题
+			</view>
+			<scroll-view :scroll-y="true" class="uni-center center-box">
+				<view v-for="(item, index) in list" :key="index" class="uni-list-item">滚动列表数据 {{ item }}</view>
+			</scroll-view>
+			<view class="">
+				底部
+			</view>
+		</uni-popup>
+	</view>
+</template>
+
+<script>
+	import uniPopup from '@/components/uni-popup/uni-popup.vue'
+
+	export default {
+		components: {
+			uniPopup
+		},
+		data() {
+			return {
+				type: '',
+				list: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+				
+			};
+		},
+		methods: {
+			togglePopup(type) {
+				this.type = type;
+			}
+		}
+	};
+</script>
+<style>
+	page {
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		background-color: #fff
+	}
+
+	view {
+		font-size: 28upx;
+		line-height: inherit
+	}
+
+	button {
+		margin: 200upx 0;
+	}
+
+
+
+	.center-box {
+		width: 500upx;
+		height: 500upx;
+	}
+
+	.uni-list-item {
+		text-align: left;
+		line-height: 80upx;
+		border-bottom: 1px #f5f5f5 solid;
+	}
+
+	.uni-list-item:last-child {
+		border: none;
+	}
+
+	.content {
+		justify-content: center;
+		text-align: center;
+		height: 500px;
+		background: #dedede;
+		flex-direction: column;
+	}
+</style>
